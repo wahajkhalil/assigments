6f9693b2ffdbc2ab92a232ac118b27f3f4a506f7
SYSTEMS\wahaj.khalil <wahaj.khalil@systemsltd.com>
2023-11-08 18:32:29 +0500

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,49 @@ import 'package:flutterapp/Screens/MenuPage.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+
+
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => CartProvider()),
         // Add other providers as needed
       ],
-      child: MyApp(),
+      child:MyApp (),
     ),
   );
+}
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Simulate the splash screen duration
+    Future.delayed(Duration(seconds: 2), () {
+      // Navigate to the next screen
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/homepageicon.jpg'), // Replace with your splash image
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -33,7 +67,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
             useMaterial3: false,
           ),
-          home: HomePage( ),
+          home: SplashScreen( ),
         )
 
      ;
@@ -58,7 +92,9 @@ class HomePage extends StatelessWidget {
           Image.asset(
             'assets/images/images.png', // Replace with your image path
             fit: BoxFit.scaleDown ,
-          ),
+          ).animate()
+        .fade(duration: 1000.ms)
+        .scale(delay: 1000.ms),
           // Overlapping button at the bottom
           Positioned(
             left: 15,
