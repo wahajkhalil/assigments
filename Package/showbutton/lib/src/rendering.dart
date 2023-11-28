d7763dfe7bbbcfc4f9b5d2ec65b3dbd13336807d
SYSTEMS\wahaj.khalil <wahaj.khalil@systemsltd.com>
2023-11-28 22:45:07 +0500

    code pushed

--- Package/showbutton/lib/src/rendering.dart
+++ Package/showbutton/lib/src/rendering.dart
@@ -4,9 +4,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Assigment 8'),
-      ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,68 @@
- import 'dart:io';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutterapp/Task/DatabaseTask.dart';
+import 'package:flutterapp/Task/PackageTask.dart';
 import 'package:showbutton/showbutton.dart';
 
-void main() {
-  HttpOverrides.global = MyHttpOverrides();
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
-}
-
-class MyHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
-  }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: Asigment(),
     );
   }
 }
+class Asigment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double buttonWidth = 200.0; // Set the desired width for the buttons
 
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: buttonWidth,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PackageTask()),
+                  );
+                },
+                child: Text('Package task'),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: buttonWidth,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                },
+                child: Text('Realtime realtime'),
+              ),
+            ),
+            // Add more buttons as needed
+          ],
+        ),
+      ),
+    );
+  }
+}
