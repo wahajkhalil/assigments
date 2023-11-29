f5989a821e81a643a1cf4ade0852d3ab2445de0c
SYSTEMS\wahaj.khalil <wahaj.khalil@systemsltd.com>
2023-11-29 18:58:08 +0500

    asisgmnet push notification fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutterapp/Helper.dart';
 import 'package:flutterapp/Task/DatabaseTask.dart';
 import 'package:flutterapp/Task/PackageTask.dart';
 import 'package:showbutton/showbutton.dart';
@@ -10,21 +12,35 @@ import 'package:showbutton/showbutton.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    _getToken();
     return MaterialApp(
       home: Asigment(),
     );
   }
 }
+Future<void> _getToken() async {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  String? token = await _firebaseMessaging.getToken();
+  print("Device Token: $token");
+}
 class Asigment extends StatelessWidget {
+
+  final NotificationService _notificationService = NotificationService();
+
+
+
   @override
   Widget build(BuildContext context) {
     double buttonWidth = 200.0; // Set the desired width for the buttons
+    _notificationService.initialize(); // Initialize notifications
 
     return Scaffold(
       appBar: AppBar(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import firebase_core
 import firebase_database
+import firebase_messaging
+import flutter_local_notifications
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -14,6 +16,8 @@ import sqflite
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
